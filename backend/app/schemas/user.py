103ef1425c143a24a9824881515dea4b33bce902
MBON-Ikariss <mbon@ikariss.fr>
2025-12-18 16:43:06 +0100

    Created project structure.

---
+++ backend/app/schemas/user.py
@@ -0,0 +1,6 @@
+from __future__ import annotations
+
+from datetime import datetime
+from typing import Optional
+
+# from sqlalchemy import Boolean, DateTime, String
--- backend/main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from ultraskate-buddy-api!")
-
-
-if __name__ == "__main__":
-    main()
